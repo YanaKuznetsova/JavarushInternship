7d67c6c4b8d31fa6226212b43442810bde40c337
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-10-15 19:54:20 +0300

    add Collection<MealWithExceed> getAllWithExceed() to MealRepository & InMemoryMealRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/repository/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/InMemoryMealRepositoryImpl.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collection;
@@ -43,6 +44,11 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
     @Override
     public Collection<Meal> getAll() {
         return repository.values();
+    }
+
+    @Override
+    public Collection<MealWithExceed> getAllWithExceed() {
+        return MealsUtil.getAllWithExceed(repository.values());
     }
 
 }
--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -1,17 +1,18 @@
 package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
 
 import java.util.Collection;
 
 public interface MealRepository {
 
     Meal save(Meal meal);
-
     void delete(int id);
-
     Meal get(int id);
-
     Collection<Meal> getAll();
+
+    Collection<MealWithExceed> getAllWithExceed();
+
 
 }
