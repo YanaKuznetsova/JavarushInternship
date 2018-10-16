276ee3384d0bc91c6d833236be396e61f0fd1699
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-10-16 18:21:03 +0300

    implementation of InMemoryUserRepositoryImpl instead of MockUserRepositoryImpl

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,81 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+
+    private static AtomicInteger idCounter = new AtomicInteger(0);
+    private static Map<Integer, User> repository = new ConcurrentHashMap<>();
+
+    @Override
+    public boolean delete(int id) {
+        log.info("delete {}", id);
+        User user = repository.remove(id);
+        return user != null;
+    }
+
+    @Override
+    public User save(User user) {
+        User result;
+        if (user.isNew()) {
+            user.setId(idCounter.getAndIncrement());
+            repository.put(user.getId(), user);
+            result = user;
+        } else {
+            result = repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
+        }
+        log.info("Save meal with id = {}", result.getId());
+        return result;
+    }
+
+    @Override
+    public User get(int id) {
+        log.info("get {}", id);
+        return repository.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        log.info("getAll");
+        List<User> users = new ArrayList<>(repository.values());
+        users.sort((user1, user2) -> {
+            if (user1 == null && user2 == null) {
+                return 0;
+            }
+            if (user1 == null) {
+                return -1;
+            }
+            if (user2 == null) {
+                return 1;
+            }
+            if (user1.getName().equals(user2.getName())) {
+                return user1.getId() - user2.getId();
+            } else {
+                return user1.getName().compareTo(user2.getName());
+            }
+        });
+        return users;
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        log.info("getByEmail {}", email);
+        return repository.values()
+                .stream()
+                .filter((u) -> u.getEmail().equals(email))
+                .findFirst().get();
+    }
+
+}
