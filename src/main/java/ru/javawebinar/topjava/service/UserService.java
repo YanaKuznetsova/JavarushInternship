ac9610244a7f4a06a743a8c4c3f4811dac30b5da
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-10-18 19:59:13 +0300

    add MealService & MealServiceImpl

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -9,15 +9,10 @@ import java.util.List;
 public interface UserService {
 
     User create(User user);
-
     void delete(int id) throws NotFoundException;
-
     User get(int id) throws NotFoundException;
-
     User getByEmail(String email) throws NotFoundException;
-
     void update(User user);
-
     List<User> getAll();
 
 }
