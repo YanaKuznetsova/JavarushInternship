4b84f57c8d3522d5c524eae943d201d9cf96aa89
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-10-24 17:27:23 +0300

    fix MealServlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -31,7 +31,7 @@ public class MealServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        springApplicationContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        springApplicationContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
         mealController = springApplicationContext.getBean(MealRestController.class);
     }
 
