09814f4131603d35afcf6572ead005ea2c224d01
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-10-29 19:27:46 +0300

    minor refactoring: delete unnecessary static init (MealsUtil, InMemoryMealRepositoryImpl) moved previously to MealTestData

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -4,10 +4,7 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.Month;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -19,22 +16,6 @@ import static java.util.stream.Collectors.toList;
 public class MealsUtil {
 
     public static final int DEFAULT_CALORIES_PER_DAY = 2000;
-    private static final LocalTime LOCAL_TIME_MIN = LocalTime.of(0, 0, 0);
-    private static final LocalTime LOCAL_TIME_MAX = LocalTime.of(23, 59, 59);
-
-    public static final List<Meal> mealsList = Arrays.asList(
-            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак",
-                    500),
-            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед",
-                    1000),
-            new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин",
-                    500),
-            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак",
-                    1000),
-            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед",
-                    500),
-            new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин",
-                    510));
 
     public static MealTo convertToExcess(Meal meal, boolean exceeded) {
         return new MealTo(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
