fc7e0cc42452c7326f4d0a4bca1dcb10d74f0c16
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-10-31 15:33:39 +0300

    add JpaUserRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -34,6 +34,12 @@ public abstract class AbstractBaseEntity {
     }
 
     @Override
+    public String toString() {
+        return String.format("Entity %s (%s)", getClass().getName(), id);
+    }
+
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
@@ -49,10 +55,4 @@ public abstract class AbstractBaseEntity {
     public int hashCode() {
         return id == null ? 0 : id;
     }
-
-    @Override
-    public String toString() {
-        return String.format("Entity %s (%s)", getClass().getName(), id);
-    }
-
 }
