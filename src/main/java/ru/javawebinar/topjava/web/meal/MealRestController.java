4fd37e0f8f1c48f87c5967119b06e1a71d1e4c1c
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-11-16 19:01:34 +0300

    add AbstractMealController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,79 +1,13 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.to.MealTo;
-import ru.javawebinar.topjava.util.MealsUtil;
-import ru.javawebinar.topjava.util.TimeUtil;
-import ru.javawebinar.topjava.util.ValidationUtil;
-import ru.javawebinar.topjava.web.SecurityUtil;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.List;
-
-import static ru.javawebinar.topjava.util.Util.orElse;
 
 @Controller
-public class MealRestController {
+public class MealRestController extends AbstractMealController {
 
-    private MealService service;
-    private static final Logger log = LoggerFactory.getLogger(MealRestController.class);
-
-    @Autowired
     public MealRestController(MealService service) {
-        this.service = service;
-    }
-
-    public Meal create(Meal meal) {
-        int userId = SecurityUtil.authUserId();
-        log.info("create meal for user = {}", userId);
-        return service.create(meal, userId);
-    }
-
-    public void update(Meal meal, int id) {
-        int userId = SecurityUtil.authUserId();
-        ValidationUtil.assureIdConsistent(meal, id);
-        log.info("update meal = {} for user = {}", id, userId);
-        service.update(meal, userId);
-    }
-
-    public void delete(int id) {
-        int userId = SecurityUtil.authUserId();
-        log.info("delete meal = {} for user = {}", id, userId);
-        service.delete(id, userId);
-    }
-
-    public Meal get(int id) {
-        int userId = SecurityUtil.authUserId();
-        log.info("get meal = {} for user = {}", id, userId);
-        return service.get(id, userId);
-    }
-
-    public List<MealTo> getAll() {
-        int userId = SecurityUtil.authUserId();
-        log.info("getAll for user = {}", userId);
-        return MealsUtil.getWithExcess(service.getAll(userId), SecurityUtil.authUserCaloriesPerDay());
-    }
-
-    public List<MealTo> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
-        int userId = SecurityUtil.authUserId();
-        log.info("getBetween dates {} - {} and times {} - {} for user = {}",
-                startDate, endDate, startTime, endTime, userId);
-        startDate = orElse(startDate, TimeUtil.MIN_DATE);
-        endDate = orElse(endDate, TimeUtil.MAX_DATE);
-        startTime = orElse(startTime, LocalTime.MIN);
-        endTime = orElse(endTime, LocalTime.MAX);
-        LocalDateTime startDateTime = LocalDateTime.of(startDate, startTime);
-        LocalDateTime endDateTime = LocalDateTime.of(endDate, endTime);
-
-        List<Meal> mealsDateFiltered = service.getBetweenDateTimes(startDateTime, endDateTime, userId);
-        return MealsUtil.getWithExcess(mealsDateFiltered, SecurityUtil.authUserCaloriesPerDay());
+        super(service);
     }
 
 }
