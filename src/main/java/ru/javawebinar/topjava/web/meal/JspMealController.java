bd2bf08dc74c671b740506a855783a99ea755b33
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-11-23 19:22:14 +0300

    refactoring of MealControllers

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -2,10 +2,7 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.util.TimeUtil;
@@ -15,60 +12,50 @@ import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 @Controller
 @RequestMapping(value = "/meals")
 public class JspMealController extends AbstractMealController {
 
     private static final String INSERT_OR_EDIT = "mealForm";
-    private static final String LIST_MEAL = "meals";
+    private static final String LIST_MEAL = "redirect:/meals";
 
     public JspMealController(MealService service) {
         super(service);
     }
 
-    //    @GetMapping("/meals")
-    @RequestMapping(method = RequestMethod.GET)
-    public String mealsList(Model model) {
-        model.addAttribute("mealsList", getAll());
-        return LIST_MEAL;
-    }
-
-    //    @GetMapping("/meals/delete/{id}")
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/delete/{id}")
     public String deleteMeal(Model model, @PathVariable int id) {
-        delete(id);
-        model.addAttribute("mealsList", getAll());
+        super.delete(id);
         return LIST_MEAL;
     }
 
-    //    @GetMapping("/meals/edit/{id}")
-    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/edit/{id}")
     public String updateMeal(Model model, @PathVariable int id) {
         Meal meal = get(id);
         model.addAttribute("mealToEdit", meal);
         return INSERT_OR_EDIT;
     }
 
-    //    @GetMapping("/meals/add")
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String insertNewMeal() {
+    @GetMapping(value = "/add")
+    public String insertNewMeal(Model model) {
+        model.addAttribute("meal", new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "", 1000));
         return INSERT_OR_EDIT;
     }
 
-    //    @PostMapping("/meals")
-    @RequestMapping(value = "/filter", method = RequestMethod.POST)
+    @PostMapping(value = "/filter")
     public String filter(Model model, @ModelAttribute Meal meal, HttpServletRequest request) throws UnsupportedEncodingException {
         LocalDate startDate = LocalDate.parse(request.getParameter("startDate"));
         LocalTime startTime = LocalTime.parse(request.getParameter("startTime"));
         LocalDate endDate = LocalDate.parse(request.getParameter("endDate"));
         LocalTime endTime = LocalTime.parse(request.getParameter("endTime"));
         request.setAttribute("mealsList", getBetween(startDate, startTime, endDate, endTime));
-        return LIST_MEAL;
+        return "meals";
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String submit(Model model, HttpServletRequest request) throws UnsupportedEncodingException {
+    @PostMapping
+    public String updateOrCreate(Model model, HttpServletRequest request) throws UnsupportedEncodingException {
         request.setCharacterEncoding("UTF-8");
         String description = request.getParameter("description");
         int calories = Integer.parseInt(request.getParameter("calories"));
@@ -77,12 +64,12 @@ public class JspMealController extends AbstractMealController {
         Integer id = stringId.isEmpty() ? null : Integer.valueOf(stringId);
         Meal meal = new Meal(dateTime, description, calories, id);
         if (meal.isNew()) {
-            create(meal);
+            super.create(meal);
         } else {
-            update(meal, id);
+            super.update(meal, id);
         }
         model.addAttribute("mealsList", getAll());
-        return "redirect:meals";
+        return LIST_MEAL;
     }
 
 }
