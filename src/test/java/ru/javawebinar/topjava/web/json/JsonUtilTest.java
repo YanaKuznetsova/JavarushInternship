9b36ef7d2987570ebcc9b649191957cfdeff2ff8
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-11-24 17:22:40 +0300

    refactor AdminRestControllerTest & ProfileRestControllerTest using jackson

---
+++ src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.web.json;
+
+import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.List;
+
+class JsonUtilTest {
+
+    @Test
+    void testReadWriteValue() throws Exception {
+        String json = JsonUtil.writeValue(ADMIN_MEAL1);
+        System.out.println(json);
+        Meal meal = JsonUtil.readValue(json, Meal.class);
+        assertMatch(meal, ADMIN_MEAL1);
+    }
+
+    @Test
+    void testReadWriteValues() throws Exception {
+        String json = JsonUtil.writeValue(MEALS);
+        System.out.println(json);
+        List<Meal> meals = JsonUtil.readValues(json, Meal.class);
+        assertMatch(meals, MEALS);
+    }
+}
