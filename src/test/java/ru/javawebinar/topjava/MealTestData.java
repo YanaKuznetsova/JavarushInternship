f03292977c44838a3ec1ecafb9c7d32db77b8c97
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-11-25 22:00:08 +0300

    add all user meals data to MealTestData

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava;
 
+import org.springframework.test.web.servlet.ResultMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.time.Month;
@@ -27,6 +28,15 @@ public class MealTestData {
             "Обед", 1000, USER_MEAL_ID + 1);
     public static final Meal USER_MEAL_2 = new Meal(of(2015, Month.MAY, 30, 20, 1),
             "Ужин", 500, USER_MEAL_ID + 2);
+    public static final Meal USER_MEAL_3 = new Meal(of(2015, Month.MAY, 31, 9, 1),
+            "Завтрак", 500, USER_MEAL_ID + 3);
+    public static final Meal USER_MEAL_4 = new Meal(of(2015, Month.MAY, 31, 13, 1),
+            "Обед", 1000, USER_MEAL_ID + 4);
+    public static final Meal USER_MEAL_5 = new Meal(of(2015, Month.MAY, 31, 17, 1),
+            "Полдник", 100, USER_MEAL_ID + 5);
+    public static final Meal USER_MEAL_6 = new Meal(of(2015, Month.MAY, 31, 20, 1),
+            "Ужин", 500, USER_MEAL_ID + 6);
+
 
     public static final List<Meal> ADMIN_MEALS = List.of(ADMIN_MEAL_2, ADMIN_MEAL_1, ADMIN_MEAL_0);
 
@@ -45,6 +55,14 @@ public class MealTestData {
 
     public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
         assertThat(actual).usingElementComparatorIgnoringFields("datetime", "user").isEqualTo(expected);
+    }
+
+    public static ResultMatcher contentJson(Meal... expected) {
+        return TestUtil.contentJson(expected);
+    }
+
+    public static ResultMatcher contentJson(Meal expected) {
+        return TestUtil.contentJson(expected);
     }
 
 }
