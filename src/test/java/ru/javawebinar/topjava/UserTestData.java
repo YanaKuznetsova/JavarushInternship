e2dcc3451adcdc0ca684decf8069274b7be9c507
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-11-25 21:59:24 +0300

    refactor: extract contentJson() to TestUtil class

--- src/test/java/ru/javawebinar/topjava/UserTestData.java
+++ src/test/java/ru/javawebinar/topjava/UserTestData.java
@@ -7,9 +7,7 @@ import ru.javawebinar.topjava.model.User;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
-import static ru.javawebinar.topjava.web.json.JsonUtil.writeIgnoreProps;
 
 public class UserTestData {
     public static final int USER_ID = START_SEQ;
@@ -31,10 +29,11 @@ public class UserTestData {
     }
 
     public static ResultMatcher contentJson(User... expected) {
-        return content().json(writeIgnoreProps(List.of(expected), "registered"));
+        return TestUtil.contentJson(expected, "registered");
     }
 
     public static ResultMatcher contentJson(User expected) {
-        return content().json(writeIgnoreProps(expected, "registered"));
+        return TestUtil.contentJson(expected, "registered");
     }
+
 }
