b859cf7acb7c15a1ea82a9d64573d23b3f62ebe0
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-11-25 15:49:56 +0300

    minor: check mediaType using parseMediaType in ResourceControllerTest

--- src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -1,22 +1,21 @@
 package ru.javawebinar.topjava.web;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.test.web.servlet.MvcResult;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.parseMediaType;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class ResourceControllerTest extends AbstractControllerTest {
 
     @Test
     void testStyleCss() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/resources/css/style.css"))
+        mockMvc.perform(get("/resources/css/style.css"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andReturn();
-        assertThat(mvcResult.getResponse().getContentType()).isEqualTo("text/css");
+                .andExpect(content().contentTypeCompatibleWith(parseMediaType("text/css")));
     }
 
 }
