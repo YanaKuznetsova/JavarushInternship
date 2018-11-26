2504ccd8902349f4092b5cadedd2045043f46a20
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-11-27 01:04:35 +0300

    add LocalDateTimeFormatter

---
+++ src/main/java/ru/javawebinar/topjava/web/converter/DateTimeFormatter.java
@@ -0,0 +1,40 @@
+package ru.javawebinar.topjava.web.converter;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Locale;
+
+import static ru.javawebinar.topjava.util.TimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.TimeUtil.parseLocalTime;
+
+public class DateTimeFormatter {
+
+    public static class LocalDateFormatter implements Formatter<LocalDate> {
+
+        @Override
+        public LocalDate parse(String text, Locale locale) throws ParseException {
+            return parseLocalDate(text);
+        }
+
+        @Override
+        public String print(LocalDate localDate, Locale locale) {
+            return localDate.format(java.time.format.DateTimeFormatter.ISO_LOCAL_DATE);
+        }
+    }
+
+    public static class LocalTimeFormatter implements Formatter<LocalTime> {
+
+        @Override
+        public LocalTime parse(String text, Locale locale) throws ParseException {
+            return parseLocalTime(text);
+        }
+
+        @Override
+        public String print(LocalTime localTime, Locale locale) {
+            return localTime.format(java.time.format.DateTimeFormatter.ISO_LOCAL_TIME);
+        }
+    }
+}
