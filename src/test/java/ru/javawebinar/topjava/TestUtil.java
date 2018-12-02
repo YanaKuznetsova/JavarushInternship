1b41ef8c377c6e85dc061cbbdcc1ca5fbefcde9e
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-12-02 22:47:22 +0300

    rest controller tests refactoring: replace comparing of json content with comparing of deserialized objects

--- src/test/java/ru/javawebinar/topjava/TestUtil.java
+++ src/test/java/ru/javawebinar/topjava/TestUtil.java
@@ -1,36 +1,33 @@
 package ru.javawebinar.topjava;
 
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
 import ru.javawebinar.topjava.web.json.JsonUtil;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static ru.javawebinar.topjava.web.json.JsonUtil.writeIgnoreProps;
-
 public class TestUtil {
 
-    public static String getContent(ResultActions action) throws UnsupportedEncodingException {
-        return action.andReturn().getResponse().getContentAsString();
+    public static String getContent(MvcResult result) throws UnsupportedEncodingException {
+        return result.getResponse().getContentAsString();
     }
 
     public static ResultActions print(ResultActions action) throws UnsupportedEncodingException {
-        System.out.println(getContent(action));
+        System.out.println(getContent(action.andReturn()));
         return action;
     }
 
-    public static <T> T readFromJson(ResultActions action, Class<T> clazz) throws UnsupportedEncodingException {
-        return JsonUtil.readValue(getContent(action), clazz);
+    public static <T> T readFromJsonResultActions(ResultActions action, Class<T> clazz) throws UnsupportedEncodingException {
+        return readFromJsonMvcResult(action.andReturn(), clazz);
     }
 
-
-    public static <T> ResultMatcher contentJson(T[] expected, String... ignoreProps) {
-        return content().json(writeIgnoreProps(List.of(expected), ignoreProps));
+    public static <T> T readFromJsonMvcResult(MvcResult result, Class<T> clazz) throws UnsupportedEncodingException {
+        return JsonUtil.readValue(getContent(result), clazz);
     }
 
-    public static <T> ResultMatcher contentJson(T expected, String... ignoreProps) {
-        return content().json(writeIgnoreProps(expected, ignoreProps));
+    public static <T> List<T> readListFromJsonMvcResult(MvcResult result, Class<T> clazz) throws UnsupportedEncodingException {
+        return JsonUtil.readValues(getContent(result), clazz);
     }
+
 }
