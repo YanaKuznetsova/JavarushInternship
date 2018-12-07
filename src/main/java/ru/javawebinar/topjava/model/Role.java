4846e48bb3d803b76f0e7a40b4339800b9c6559e
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-12-07 18:45:21 +0300

    add custom authorization provider

--- src/main/java/ru/javawebinar/topjava/model/Role.java
+++ src/main/java/ru/javawebinar/topjava/model/Role.java
@@ -1,8 +1,15 @@
 package ru.javawebinar.topjava.model;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
 
     ROLE_USER,
-    ROLE_ADMIN
+    ROLE_ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 
 }
