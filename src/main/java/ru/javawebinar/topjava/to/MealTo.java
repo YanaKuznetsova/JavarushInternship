42d1cc20d1275b251a09a77668684d1c32ba874b
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-12-10 17:56:12 +0300

    add validation of MealTo

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -1,13 +1,24 @@
 package ru.javawebinar.topjava.to;
 
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class MealTo extends BaseTo {
 
+    @NotNull
     private LocalDateTime dateTime;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
     private String description;
+
+    @Range(min = 10, max = 10000)
     private int calories;
     private boolean excess;
 
