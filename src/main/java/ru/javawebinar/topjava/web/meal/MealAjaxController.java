2c3a1dcdf2cf212da9a0b4895db1b0bb5449c332
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2018-12-17 17:32:00 +0300

    add exception handling and showing to user

--- src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -35,6 +35,7 @@ public class MealAjaxController extends AbstractMealController {
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public ResponseEntity<String> createOrUpdate(@Valid Meal meal, BindingResult result) {
         if (result.hasErrors()) {
+            // TODO change to exception handler
             return ValidationUtil.getErrorResponse(result);
         }
         if (meal.isNew()) {
